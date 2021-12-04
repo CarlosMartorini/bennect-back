4e5f73b77aae71ea7168561265fbec8c65be9546
Carlos Martorini <c7.silveira@gmail.com>
2021-12-04 10:40:46 -0300

    Config settings and urls project

--- bennect/urls.py
+++ bennect/urls.py
@@ -14,8 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from django.conf.urls.static import static
+from django.conf import settings
+
+from rest_framework import routers
+from accounts.views import UserViewSet
+
+route = routers.DefaultRouter()
+
+route.register(r'accounts', UserViewSet, basename='users')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', include(route.urls))
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
